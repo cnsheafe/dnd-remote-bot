ae07fbc9a0bdb90cb5f36de4f0c2c1c23622824b
cnsheafe <cnsheafe@gmail.com>
2017-04-06 17:01:24 -0500

    first push

---
+++ web-client/app.js
@@ -0,0 +1,14 @@
+/*jshint esversion:6*/
+
+function postControlData(ctrlObj) {
+  let settings = {
+    
+  };
+}
+
+$(function main() {
+  let ctrlObj = {
+    turnLeft: true //false means turn right
+  };
+
+});
