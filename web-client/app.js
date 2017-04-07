df5cff6910193437869be0eae6966eb6abe29173
cnsheafe <cnsheafe@gmail.com>
2017-04-06 19:17:44 -0500

    ajax code for PUT data

--- web-client/app.js
+++ web-client/app.js
@@ -2,13 +2,21 @@
 
 function postControlData(ctrlObj) {
   let settings = {
-    
+    method: 'PUT',
+    url: 'http://50-81-20-64-client.mchsi.com',
+    data: ctrlObj
   };
+  $.ajax(settings);
 }
 
 $(function main() {
+
   let ctrlObj = {
     turnLeft: true //false means turn right
   };
+
+  $('#left').on('submit',function(event) {
+    postControlData(ctrlObj);
+  });
 
 });
